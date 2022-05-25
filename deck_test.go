94ef5a150c0b8dcbe7dd4de826957d20a32cf425
jordi <vilagutjordi@gmail.com>
2022-05-25 10:45:20 +0200

    Add test

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	deck := newDeck()
+	if len(deck) != 48 {
+		t.Errorf("Invalid amount of cards: %d", len(deck))
+	}
+}
+
+func TestNewDeckFromFile(t *testing.T) {
+	deck := newDeck()
+	deck.saveToFile("cards")
+	deckFromFile := newDeckFromFile("cards")
+
+	if len(deckFromFile) != len(deck) {
+		t.Error("Deck from file has different length.")
+	}
+
+	if deck[0] != deckFromFile[0] {
+		t.Error("First element differs in deck from file.")
+	}
+
+	deleteFile("cards")
+}
